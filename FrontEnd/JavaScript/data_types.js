e9a452fa8498512f555b0dcaf95b365b93f3b017
Lakshay Nasa <76848292+lakshay-nasa@users.noreply.github.com>
2022-02-07 18:51:19 +0530

    Add: created data_types.js

---
+++ FrontEnd/JavaScript/data_types.js
@@ -0,0 +1,6 @@
+// Data Types in JavaScript
+/*
+In JavaScript, data types are split in two categories
+1. Primitive - numbers, strings, booleans, null, undefined, and symbols
+2. Refrence - Arrays and Objects
+/*

068cc900fe4dd31430aaedc01b8994d591c01bd8
lakshay-nasa <lakshay1717@gmail.com>
2022-03-13 13:37:13 +0530

    Feat: 🚀 Added Condition and loops in JS

--- FrontEnd/JavaScript/if_else.js
+++ FrontEnd/JavaScript/if_else.js
@@ -1,1 +1,34 @@
-//if else
+//Conditional statements are used to execute sepecfic task based on different conditions.
+
+
+
+// if-else statement --->
+
+var Score = 87;
+var high_score = 100
+if(Score>40){
+    console.log('You passed the Level'); // If condition is true
+}
+else{
+    console.log('Try Again!') // If is false.
+}
+
+
+
+// if-else Ladder --->
+
+if(condition1){
+    console.log(1) //If condtion1 is true, it will return 1
+}
+else if(condition2){
+    console.log(2) //If above condition fails and condtion2 is true, it will return 2
+
+}
+else if(condition3){
+    console.log(3) //If above condition fails and condtion3 is true ab it will return 3
+
+}
+else{
+    console.log(4) //If nothing is true, it will return 'else' i.e 4.
+
+}

0aa65cb3b8b61dbcd137174b31a7cf20d2afffcf
Lakshay Nasa <76848292+lakshay-nasa@users.noreply.github.com>
2022-06-25 23:59:22 +0530

    Feat: Updated WeatherApp

--- BackEnd/Flask/Projects/Weather App/app.py
+++ BackEnd/Flask/Projects/Weather App/app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-API_Key = "Secret_Key"
+API_Key = "d670c867d8c8998117536ef9d9abe6fb"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -20,20 +20,21 @@ def home():
             
             # Data for Weather
             data = response.json()
-            temp = round(data['main']['temp'] - 273.15) + "&#8451"
+            temp = round(data['main']['temp'] - 273.15)
             pressure = data['main']['pressure']
             humidity = data['main']['humidity']
             iconData = data['weather'][0]['icon']
             weatherDesc = data['weather'][0]['description']
             weatherIcon = f"https://openweathermap.org/img/wn/{iconData}.png"
+            
 
-            return render_template('index.html', caption1="Weather in", temp=temp, caption2= "Humidity",humidity=humidity, caption3= "Pressure", pressure=pressure, city=city, src=getImage(), Icon=weatherIcon, wDesc=weatherDesc)
+            return render_template('index.html', caption1="Weather in", temp=f"{temp} ℃", caption2= "Humidity: ",humidity=humidity, caption3= "Pressure: ", pressure=pressure, city=city, src=getImage(), Icon=weatherIcon, wDesc=weatherDesc)
 
         except:
-            return render_template('index.html')
+            return render_template('index.html',src=getImage(), Icon=None)
 
-
-    return render_template('index.html', src="https://api.unsplash.com/photos/random/?client_id=c5AijfSm_8b_JsYU3kuYBtTRdM9Lj_KdOsveYSZQzL8")
+    else:
+        return render_template('index.html', src=getImage(), Icon=None)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500, debug=True)
--- BackEnd/Flask/Projects/Weather App/bgImage.py
+++ BackEnd/Flask/Projects/Weather App/bgImage.py
@@ -1,12 +1,12 @@
 from ast import Try
 from flask import Flask, flash, render_template, request
 import requests
-import random
+import os
 
 app = Flask(__name__)
 
 # Access Key
-client_id = "Access key"
+client_id = "c5AijfSm_8b_JsYU3kuYBtTRdM9Lj_KdOsveYSZQzL8"
 
 @app.route('/', methods=['GET', 'POST'])
 def getImage():
@@ -14,8 +14,6 @@ def getImage():
         try:
             location = request.form['city']
             # API for bg-Image
-            # imageKeywordAPI = f"https://api.unsplash.com/search/photos?query={location}&page{random.randint(0, 1000)}&client_id={client_id}"
-
             imageAPI = f"https://api.unsplash.com/photos/random/?query={location}&client_id={client_id}"
 
             # Response for bg-Image
@@ -29,8 +27,8 @@ def getImage():
             return bg_image
         
         except:
-            bg_image = "https://api.unsplash.com/photos/random/?client_id={client_id}"
-            
+            bg_image = '/static/Images/bg-image.jpg'
             return bg_image
-
-    
+    else:
+        bg_image = '/static/Images/bg-image.jpg'
+        return bg_image

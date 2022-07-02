fc0dd785177b0d0c13770426710710c3f4ad36fd
Lakshay Nasa <76848292+lakshay-nasa@users.noreply.github.com>
2022-07-03 04:12:02 +0530

    Feat:✨ Updated REST API

--- BackEnd/Flask/Projects/REST API/app.py
+++ BackEnd/Flask/Projects/REST API/app.py
@@ -1,21 +1,48 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from requests import request
+import requests
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False #This will Disable sorting app-wide
 
+s_no = []
+id = []
+for i in range(1,31):
+    s_no.append(i)
+    id.append(f"dMinion0{i}")
+    i+=1
 
-@app.route('/<string:n>')
-def home(n):
-    if True:
-        Restaurent = {
-            "itemName" : n,
-            "restaurentName" : "Rinsho",
-        }
+minions_list = ["Bob", "Carl", "Darwin", "Dave", "Frank", "Jerry", "John","Kevin", "Ken", "Lance", "Larry","Mark", "Mike", "Nobert", "Paul", "Phil", "Steve", "Stuart", "Tim", "Tom", "Chris", "Mel", "Jorge", "Donny"]
 
-        return jsonify(Restaurent)
+food = ["bananas"]
+
+m_data = []
+
+for i in range(len(minions_list)):
+    if minions_list[0] == True:
+        Hair ="no"
     else:
-        pass
+        Hair = "yes"
 
+    details = {
+        "s no.": s_no[i],
+        "name": minions_list[i],
+        "id": id[i],
+        "favourite food" : food[0],
+        "hair": Hair,
+    }
+    i += 1
+    m_data.append(details)
+
+
+
+@app.route('/')
+def home():
+    return "<h1> Hello World </h1>"
+
+@app.route('/minions')
+def  minions():
+    return jsonify({'Minions':m_data})
 
 if __name__ == "__main__":
     app.run(debug=True)
